05025e9cabeee63a9d458d5670a357b5827d0aed
ermi <ermiasmulugeta71@gmail.com>
2026-05-12 15:54:23 +0300

    Refactor PHP code to improve readability and add comments for clarity

--- 1.php
+++ 1.php
@@ -8,12 +8,14 @@
 <body>
     <<h1>my first php page</h1>
     <?php
+        // welcome to php programming
         echo "hello world";
         echo "<br>";
+        // sum of two numbers
         $sum = 5 + 10;
         echo "the sum of 5 and 10 is: " . $sum;
         echo "<br>";
-
+        // if condition
         if ($sum > 10) {
             echo "the sum is greater than 10";
         } else if ($sum == 10) {
@@ -23,7 +25,7 @@
         }
 
         echo "<br>";
-
+        // for loop with different syntax
         for($counter = 0 ; $counter < 5; $counter++) {
             echo '<h3>the counter is: ' . $counter . '</h3>';
         }
@@ -41,16 +43,19 @@
             ?>
             <h3>the counter is: <?= $counter; ?></h3>
             <?php endfor;
+
+        // foreach loop array
         $names = ["abebe","kebede","chala"];
         foreach ($names as $name){
             echo '<h3> '.$name.'</h3>';
         } 
-
+        // while loop
         $counter = 0;
         while ($counter < 10) {
             echo '<h3>the counter is: ' . $counter . '</h3>';
             $counter++;
         }
+        // do while loop
         echo "<br>";
         $counter = 0;
         do {
@@ -59,10 +64,23 @@
         } while ($counter < 10);
 
         echo "<br>";
+        // function definition and call
         function add($a, $b) {
             return $a + $b;
         }
         echo "the sum of 5 and 10 is: " . add(5, 10);
+
+        // index array
+        $students = ["abebe","kebede","chala"];
+        echo $students[0]; // abebe
+        $students[1] = "tadesse"; // change kebede to tadesse
+        for($i=0; $i < count($students); $i++){
+            echo '<h3> the student is: '.$students[$i].'</h3>';
+        }
+        //assosiative array
+        $students = ["abebe","kebede","chala"];
+        $age = array("abebe" => 20, "kebede" => 22, "chala" => 21);
+        echo "the age of abebe is: " . $age["abebe"];
     ?>
 </body>
 </html>
