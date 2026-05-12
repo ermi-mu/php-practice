c1d32993ef0ffa724e2dd7199d14596acf5faa35
ermi <ermiasmulugeta71@gmail.com>
2026-05-12 14:56:04 +0300

    Fix HTML header tag and improve PHP sum calculation output

--- 1.php
+++ 1.php
@@ -9,8 +9,18 @@
     <<h1>my first php page</h1>
     <?php
         echo "hello world";
+        echo "<br>;
         $sum = 5 + 10;
         echo "<br>the sum of 5 and 10 is: " . $sum;
+        echo "<br>";
+
+        if ($sum > 10) {
+            echo "the sum is greater than 10";
+        } else if ($sum == 10) {
+            echo "the sum is equal to 10";
+        } else {
+            echo "the sum is less than 10";
+        }
     ?>
 </body>
 </html>
